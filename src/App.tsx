d2fcd171bd9fb953d84e757c851c87895e2892bb
Nikhil <bnikkhil14@gmail.com>
2026-08-25 01:59:04 -0400

    Add GitHub Pages deployment workflow

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/devops-learning-platform">
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
